15734b0443ad5fe899353eff87ad492b25309b6b
Jorge Samuel Teixeira Jordão <116199476+JorgeSTJordao@users.noreply.github.com>
2025-04-13 20:16:46 -0300

    feat: timeline de 2024 criada

--- portfolio/my_portfolio/views.py
+++ portfolio/my_portfolio/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def timeline(request):
+    return render(request, "routes/timeline.html")

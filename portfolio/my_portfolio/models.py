165368f7ed059ca23cf16b219574d40ff7bf6f44
Jorge Samuel Teixeira Jordão <116199476+JorgeSTJordao@users.noreply.github.com>
2025-04-14 21:37:21 -0300

    feat: adicionado botão de download de currículo

--- portfolio/my_portfolio/models.py
+++ portfolio/my_portfolio/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=200)
+    descricao = models.TextField()
+    data_inicio = models.DateField()
+    concluido = models.BooleanField(default=False)
+    ano = models.IntegerField()
+    ordem = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['ano', 'ordem']
+
+    def __str__(self):
+        return f"{self.titulo} ({self.data_inicio})"
--- portfolio/my_portfolio/views.py
+++ portfolio/my_portfolio/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import Evento
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
 
 def timeline(request):
-    return render(request, "routes/timeline.html")
+    eventos = Evento.objects.all().order_by('ordem')
+    context = {'eventos': eventos}
+
+    return render(request, 'routes/timeline.html', context)
